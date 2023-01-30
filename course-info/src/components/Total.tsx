0f0f459affb064818dc741862982d8d50a979a7e
zillur-rgb <zillurdeu@gmail.com>
2023-01-30 08:30:26 +0100

    2.4

--- course-info/src/components/Total.tsx
+++ course-info/src/components/Total.tsx
@@ -4,7 +4,15 @@ const Total = () => {
   const totalEx = courses.reduce((total, course) => {
     return (total = total + course.exercise);
   }, 0);
-  return <p>Number of exercises {totalEx}</p>;
+  return (
+    <p
+      style={{
+        fontWeight: 'bold',
+      }}
+    >
+      Number of exercises {totalEx}
+    </p>
+  );
 };
 
 export default Total;
