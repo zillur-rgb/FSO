82a776c81547aa9ab09c60ae81b1ad3e9b74fb46
zillur-rgb <zillurdeu@gmail.com>
2023-02-03 23:00:51 +0100

    2.8 done: Added new number fiels

--- phonebook/src/App.tsx
+++ phonebook/src/App.tsx
@@ -1,20 +1,28 @@
 import { useState } from "react";
 
+export type PhonebookType = {
+  name: string;
+  number: string;
+}[];
+
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [persons, setPersons] = useState<PhonebookType>([]);
   const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
   console.log("Persons:", persons);
 
   const onSubmit = (e: { preventDefault: () => void }) => {
     e.preventDefault();
     const addName = {
       name: newName,
+      number: newNumber,
     };
     const alreadyAdded = persons.find((person) => person.name === addName.name);
     alreadyAdded
       ? alert(`${addName.name} is already added`)
       : setPersons(persons.concat(addName));
     setNewName("");
+    setNewNumber("");
   };
 
   console.log("newName: ", newName);
@@ -24,8 +32,16 @@ const App = () => {
       <h2>Phonebook</h2>
       <form onSubmit={onSubmit}>
         <div>
-          name:{" "}
+          Name:{" "}
           <input value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          Number:{" "}
+          <input
+            type="number"
+            value={newNumber}
+            onChange={(e) => setNewNumber(e.target.value)}
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -33,7 +49,9 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((person, idx) => (
-        <p key={idx}>{person.name}</p>
+        <p key={idx}>
+          {person.name} : {person.number}
+        </p>
       ))}
       typing {newName}
     </>
