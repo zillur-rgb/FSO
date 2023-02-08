b30ffc48f3a6ed38069f3a9473a30cc3932d1b9a
zillur-rgb <zillurdeu@gmail.com>
2023-02-08 05:20:09 +0100

    Refactroing: Added Schema file

---
+++ blog-backend-latter/models/blog.js
@@ -0,0 +1,41 @@
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+/**
+ * Schema declaration for blog
+ */
+const blogSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      min: [6, "Must be at least 6 but got {VALUE}"],
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: [String],
+    desc: {
+      type: String,
+      min: 12,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+/**
+ * Removing _id and _v from the schema so that we can
+ * filter based on id instead of _id. However __id and
+ * __v will still be included in the main database collection.
+ */
+blogSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
