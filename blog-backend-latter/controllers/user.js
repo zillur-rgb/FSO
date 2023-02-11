d5cd64c173156656a1d3f339419513fa0dab486f
zillur-rgb <zillurdeu@gmail.com>
2023-02-11 23:52:00 +0100

    4.16 done: Validation added and proper error msg

--- blog-backend-latter/controllers/user.js
+++ blog-backend-latter/controllers/user.js
@@ -3,25 +3,43 @@ const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
 const User = require("../models/user");
 
-usersRouter.post("/", async (req, res) => {
-  const { username, name, password } = req.body;
+usersRouter.post("/", async (req, res, next) => {
+  try {
+    const { username, name, password } = req.body;
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(password, saltRounds);
+    if (!password && !password.length > 3) {
+      return res.status(400).json({
+        error: "Password must be at least 3 characters long.",
+      });
+    }
 
-  const user = new User({
-    username,
-    name,
-    passwordHash,
-  });
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
 
-  const savedUser = await user.save();
+    const user = new User({
+      username,
+      name,
+      passwordHash,
+    });
 
-  res.status(201).json(savedUser);
+    const savedUser = await user.save();
+
+    res.status(201).json(savedUser);
+  } catch (error) {
+    // console.log("errorllalal", error);
+    next(error.name);
+  }
 });
 usersRouter.get("/", async (req, res) => {
   const users = await User.find({});
   res.json(users);
+});
+
+usersRouter.delete("/:id", async (req, res, next) => {
+  const id = req.params.id;
+
+  const deletedId = await User.findByIdAndDelete(id);
+  res.status(204).end();
 });
 
 module.exports = usersRouter;
