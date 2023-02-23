e82568ebc34ef676b8806c6f2b06bc0e2af4a2f4
zillur-rgb <zillurdeu@gmail.com>
2023-02-23 23:24:16 +0100

    ex 5.7 done: Show/ hide description

--- blog-frontend/src/components/Blog.tsx
+++ blog-frontend/src/components/Blog.tsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
+
 export interface BlogType {
   title: string;
   desc: string;
 }
 
-const Blog = ({ title, desc }: BlogType) => (
-  <div key={desc}>
-    {title} {desc}
-  </div>
-);
+const Blog = ({ title, desc }: BlogType) => {
+  const [viewDesc, setViewDesc] = useState<boolean>(false);
+
+  const blogStyle: {
+    paddingTop: number;
+    paddingLeft: number;
+    border: string;
+    borderWidth: number;
+    marginBottom: number;
+  } = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  return (
+    <div key={desc} style={blogStyle}>
+      <h3>{title}</h3>
+      <button onClick={() => setViewDesc(!viewDesc)}>
+        {viewDesc ? "hide" : "show"} description
+      </button>
+      {viewDesc && <p>{desc}</p>}
+    </div>
+  );
+};
 
 export default Blog;
